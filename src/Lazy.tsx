525c19dba0d9c9d77fa60e06beba39b7e43728f5
Sam Barrow <sam@sambarrow.com>
2023-02-20 21:25:24 -0500

    .

--- src/Lazy.tsx
+++ src/Lazy.tsx
@@ -1,12 +1,65 @@
 
 import { ComponentType, createElement, Fragment, useContext, useEffect, useRef, useState } from "react"
-import { LazyContext, LazyOverrides } from "."
+import { LazyContext, LazyOptions, LazyOverrides } from "."
 import { defaultLazyOptions, processLazyOptions, useDelayed } from "./internal"
 
 export type LazyProps<D, P> = LazyOverrides<P> & {
     pass: P
     component: ComponentType<P & D>//TODO does component need to accept both? thought we could just merge P with D.
     result?: PromiseSettledResult<D>
+}
+
+type RenderLazyArguments<P, D> = {
+    result: PromiseSettledResult<D> | undefined
+    pass: P
+    render: (value: D) => JSX.Element
+    overrides?: LazyOverrides<P>
+}
+
+//TODO rm?
+export function renderLazy<D, P extends {}>(args: RenderLazyArguments<P, D>) {
+    const defaults = useContext(LazyContext)
+    const isFirstMount = useRef(true)
+    const [prev, setPrev] = useState(args.result)
+    useEffect(() => {
+        if (isFirstMount.current) {
+            isFirstMount.current = false
+        }
+        else {
+            if (args.result !== undefined) {
+                setPrev(args.result)
+            }
+        }
+    }, [args.result])
+    // }, [props.result === undefined])//TODO shouldnt this update if props.result changes at all?
+    const options = processLazyOptions({ ...defaultLazyOptions, ...defaults, ...args.overrides }, args.pass)
+    const data = args.result ?? prev
+    const isLoading = data === undefined || (args.result === undefined && !options.distinguishReloading)
+    const isReloading = args.result === undefined
+    const loadingReady = useDelayed(isLoading ? options.loadingDelay : 0)
+    const reloadingReady = useDelayed(isReloading ? options.reloadingDelay : 0)
+    if (isLoading) {
+        if (!options.showLoading || !loadingReady) {
+            return <Fragment />
+        }
+        return createElement(options.onLoading, { ...args.pass, title: options.loadingTitle })
+    }
+    else {
+        return createElement(options.onReloading, {
+            ...args.pass,
+            reloading: options.showReloading && isReloading && reloadingReady,
+            title: options.reloadingTitle,
+            children: (() => {
+                if (data.status === "rejected") {
+                    //return options.onError({ ...pass, error: data.reason })
+                    return createElement(options.onError, { ...args.pass, error: data.reason })
+                }
+                else {
+                    return args.render(data.value)
+                }
+            })()
+        })
+    }
 }
 
 export function Lazy<D extends {}, P extends {}>(props: LazyProps<D, P>) {
@@ -22,8 +75,9 @@ export function Lazy<D extends {}, P extends {}>(props: LazyProps<D, P>) {
                 setPrev(props.result)
             }
         }
-    }, [props.result === undefined])
-    const options = processLazyOptions({ ...defaultLazyOptions, ...defaults, ...props }, props.pass)
+    }, [props.result])
+    // }, [props.result === undefined])//TODO shouldnt this update if props.result changes at all?
+    const options = processLazyOptions({ ...defaultLazyOptions, ...defaults, ...props as LazyOptions<P> }, props.pass)
     const data = props.result ?? prev
     const isLoading = data === undefined || (props.result === undefined && !options.distinguishReloading)
     const isReloading = props.result === undefined
--- src/withObservable.tsx
+++ src/withObservable.tsx
@@ -4,7 +4,7 @@ import { Observable } from "rxjs"
 import { LazyOverrides, withLazy } from "./"
 import { addKeyToPromiseResult } from "./internal"
 
-export function useObservableResult<D>(observable: Observable<D>) {
+export function useObservableLazy<D>(observable: Observable<D>) {
     const [result, setResult] = useState<PromiseSettledResult<D>>()
     useEffect(() => {
         const sub = observable.subscribe({ next: value => setResult({ status: "fulfilled", value }), error: reason => setResult({ status: "rejected", reason }) })
@@ -15,10 +15,29 @@ export function useObservableResult<D>(observable: Observable<D>) {
     return result
 }
 
+export function useObservableResultLazy<D>(observable: Observable<PromiseSettledResult<D> | undefined>) {
+    const [result, setResult] = useState<PromiseSettledResult<D>>()
+    useEffect(() => {
+        const sub = observable.subscribe({ next: value => setResult(value), error: reason => setResult({ status: "rejected", reason }) })
+        return () => {
+            sub.unsubscribe()
+        }
+    }, [observable])
+    return result
+}
+
 export function withObservable<I extends {}, D extends {}>(factory: (props: I) => Observable<D>, overrides: LazyOverrides<I> = {}) {
-    return withLazy((props: I) => ({ result: useObservableResult(factory(props)), pass: {} }), overrides)
+    return withLazy((props: I) => ({ result: useObservableLazy(factory(props)), pass: {} }), overrides)
+}
+
+export function withObservableResult<I extends {}, D extends {}>(factory: (props: I) => Observable<PromiseSettledResult<D> | undefined>, overrides: LazyOverrides<I> = {}) {
+    return withLazy((props: I) => ({ result: useObservableResultLazy(factory(props)), pass: {} }), overrides)
 }
 
 export function withObservableAs<I extends {}, D, K extends string>(key: K, factory: (props: I) => Observable<D>, overrides: LazyOverrides<I> = {}) {
-    return withLazy((props: I) => ({ result: addKeyToPromiseResult(key, useObservableResult(factory(props))), pass: {} }), overrides)
+    return withLazy((props: I) => ({ result: addKeyToPromiseResult(key, useObservableLazy(factory(props))), pass: {} }), overrides)
+}
+
+export function withObservableResultAs<I extends {}, D, K extends string>(key: K, factory: (props: I) => Observable<PromiseSettledResult<D> | undefined>, overrides: LazyOverrides<I> = {}) {
+    return withLazy((props: I) => ({ result: addKeyToPromiseResult(key, useObservableResultLazy(factory(props))), pass: {} }), overrides)
 }
